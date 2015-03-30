fda8d088999ff503e26c98d00ad0bcaaed9c93de
Martin Lehmann <lehmar11@student.westerdals.no>
2015-03-30 12:32:17 +0200

    Add initial sessions endpoint.

--- server-node/controllers/sessions.js
+++ server-node/controllers/sessions.js
@@ -1,7 +1,10 @@
 var router = require('express').Router();
+var jwt = require('jwt-simple');
+
+var jwtSecret = require('../secrets').jwt;
 
 router.get('/', function(req, res) {
-    res.send('hi');
+    res.send(jwt.encode({username: 'theneva'}, jwtSecret));
 });
 
 module.exports = router;
