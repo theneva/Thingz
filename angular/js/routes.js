c39d6dd18f45493eb3a025cf8f00840aa8c3d8f2
Martin Lehmann <lehmar11@student.westerdals.no>
2015-04-03 10:40:06 +0200

    Restructure routes (remove '/things' everywhere

--- angular/js/routes.js
+++ angular/js/routes.js
@@ -1,9 +1,9 @@
 angular.module('thingz').config(function ($routeProvider) {
     $routeProvider
         .when('/', {controller: 'MyThingsController', templateUrl: '/things/list.html'})
-        .when('/things/:id', {controller: 'ThingController', templateUrl: '/things/show.html'})
-        .when('/new-thing', {controller: 'NewThingController', templateUrl: '/things/new.html'})
+        .when('/new', {controller: 'NewThingController', templateUrl: '/things/new.html'})
         .when('/register', {controller: 'RegisterController', templateUrl: '/users/register.html'})
         .when('/login', {controller: 'LoginController', templateUrl: '/login.html'})
+        .when('/:id', {controller: 'ThingController', templateUrl: '/things/show.html'})
         .otherwise({controller: 'NotFoundController', templateUrl: '/not_found.html'});
 });
